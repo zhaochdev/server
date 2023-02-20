f11cf7e0377dc50f38df38524b0fdecaa8aa1867
zhaochdev <zhaoch.dev@outlook.com>
2023-02-20 15:54:41 +0800

    init commit

---
+++ src/main/java/com/edutils/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.edutils.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
